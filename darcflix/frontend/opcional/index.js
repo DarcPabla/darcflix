1e8cfff8a62c9458bd1a235aba06dcb05d0685b3
Wesley Rocha <wesleysr1997@gmail.com>
2022-12-04 14:33:53 -0400

    Formata arquivos

--- darcflix/frontend/opcional/index.js
+++ darcflix/frontend/opcional/index.js
@@ -1,10 +1,10 @@
-function criarLista(imgPath, id){
-  const ul = document.querySelector('#lista-filmes')
-  ul.setAttribute("style", "list-style-type: none")
-  ul.setAttribute("class", "row")
+function criarLista(imgPath, id) {
+  const ul = document.querySelector("#lista-filmes");
+  ul.setAttribute("style", "list-style-type: none");
+  ul.setAttribute("class", "row");
 
-  const div = document.createElement('div')
-  div.setAttribute("class", "col-md-3")
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-md-3");
 
   div.innerHTML = `
     <li>
@@ -12,77 +12,72 @@ function criarLista(imgPath, id){
         <img src="${imgPath}" style="width: 200px; heigth: 300px" id=${id}/>
       </a>
     </li>
-  `
+  `;
 
-  ul.appendChild(div)
+  ul.appendChild(div);
 }
 
-async function percorrerDB(){
+async function percorrerDB() {
   let database;
-  await fetch("http://localhost:3000/filmes").then((res)=>res.json()).then((data)=>{
-    database = data
-  })
-  
-  for(let i of database) {
+  await fetch("http://localhost:3000/filmes")
+    .then((res) => res.json())
+    .then((data) => {
+      database = data;
+    });
 
-    if(i.id > database.length - 4){
-      criarLista(i.img, i.id)
-
+  for (let i of database) {
+    if (i.id > database.length - 4) {
+      criarLista(i.img, i.id);
     }
 
-    if(i.id < 5){
-      criarListaUltimos(i.img, i.id)
-
+    if (i.id < 5) {
+      criarListaUltimos(i.img, i.id);
     }
 
-
-    if(i.id < 5){
-criarListaEmAlta(i.img, i.id)
+    if (i.id < 5) {
+      criarListaEmAlta(i.img, i.id);
     }
-   
-    
   }
 }
 
-async function salvar(){
-  const nome = document.querySelector("#nome").value
-  const sinopse = document.querySelector("#sinopse").value
-  const autor = document.querySelector("#autor").value
-  const img = document.querySelector("#link-imagem").value
+async function salvar() {
+  const nome = document.querySelector("#nome").value;
+  const sinopse = document.querySelector("#sinopse").value;
+  const autor = document.querySelector("#autor").value;
+  const img = document.querySelector("#link-imagem").value;
 
   const body = {
     nome,
     sinopse,
     autor,
-    img
-  }
+    img,
+  };
 
-  var options = { method: 'POST',
-               mode: 'cors',
-               headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(body)
-              };
+  var options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
 
-  await fetch("http://localhost:3000/filmes/", options)
+  await fetch("http://localhost:3000/filmes/", options);
 
-  nome.innerHTML = ""
-  sinopse.innerHTML = ""
-  autor.innerHTML = ""
-  img.innerHTML = ""
-
-
+  nome.innerHTML = "";
+  sinopse.innerHTML = "";
+  autor.innerHTML = "";
+  img.innerHTML = "";
 }
 
-function criarListaUltimos(imgPath, id){
-  const ul = document.querySelector('#ultimos-filmes')
-  ul.setAttribute("style", "list-style-type: none")
-  ul.setAttribute("class", "row")
+function criarListaUltimos(imgPath, id) {
+  const ul = document.querySelector("#ultimos-filmes");
+  ul.setAttribute("style", "list-style-type: none");
+  ul.setAttribute("class", "row");
 
-  const div = document.createElement('div')
-  div.setAttribute("class", "col-md-3")
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-md-3");
 
   div.innerHTML = `
     <li>
@@ -90,20 +85,18 @@ function criarListaUltimos(imgPath, id){
         <img src="${imgPath}" style="width: 200px; heigth: 300px" id=${id}/>
       </a>
     </li>
-  `
+  `;
 
-  ul.appendChild(div)
-
-
+  ul.appendChild(div);
 }
 
-function criarListaEmAlta(imgPath, id){
-  const ul = document.querySelector('#lista-filmes-alta')
-  ul.setAttribute("style", "list-style-type: none")
-  ul.setAttribute("class", "row")
+function criarListaEmAlta(imgPath, id) {
+  const ul = document.querySelector("#lista-filmes-alta");
+  ul.setAttribute("style", "list-style-type: none");
+  ul.setAttribute("class", "row");
 
-  const div = document.createElement('div')
-  div.setAttribute("class", "col-md-3")
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-md-3");
 
   div.innerHTML = `
     <li>
@@ -111,13 +104,9 @@ function criarListaEmAlta(imgPath, id){
         <img src="${imgPath}" style="width: 200px; heigth: 300px" id=${id}/>
       </a>
     </li>
-  `
+  `;
 
-  ul.appendChild(div)
+  ul.appendChild(div);
 }
- 
 
-
-
-percorrerDB()
-
+percorrerDB();
